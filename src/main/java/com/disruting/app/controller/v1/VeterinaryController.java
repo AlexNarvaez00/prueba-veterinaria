346f9ef4c63135e5bbcca7a91d061453cfed3450
AlexNarvaez00 <zaizealexis@gmail.com>
2023-08-14 14:29:30 -0600

    note: Last changes

--- src/main/java/com/disruting/app/controller/v1/VeterinaryController.java
+++ src/main/java/com/disruting/app/controller/v1/VeterinaryController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,6 +41,12 @@ public class VeterinaryController {
   @GetMapping("/{id}")
   public Veterinary getOneVeterinary(@PathVariable("id") UUID id) {
     return this.repo.findById(id).orElseThrow(() -> new VeterinaryNotFoundException(id));
+  }
+
+  @PutMapping("/{id}")
+  public void updateVeterinary(@PathVariable("id") UUID id, @RequestBody @Valid Veterinary veterinary ) {
+    veterinary.setId(id);
+    this.repo.save(veterinary);
   }
 
   @PostMapping("/create")
